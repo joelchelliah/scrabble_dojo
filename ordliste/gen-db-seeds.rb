da68b9d2eefb4d0d330e8ba708c4f1b4c0726520
Joel Chelliah <joel.chelliah@bekk.no>
2013-09-20 19:53:57 +0200

    updated seed data with check to see if row already exists

--- ordliste/gen-db-seeds.rb
+++ ordliste/gen-db-seeds.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby
 
-# Generates record creation lines for initializing the database.
+# Generates seed data for initializing the database.
 # All words are taken from the NSF-ordliste.
 # Duplicate words and words of length greater than 15 are left out.
 
@@ -11,7 +11,9 @@ File.open('NSF-ordlisten.txt', 'r') do |input|
       word = line.split[0]
       unless word.length > 15 or last_word == word
         letters = word.split(//)
-        output << "Word.create(text: '#{word}', letters: '#{letters.sort.join}', length: #{word.length}, first_letter: '#{letters.first}')\n"
+        output << "Word.create(text: '#{word}', letters: '#{letters.sort.join}', length: #{word.length}, first_letter: '#{letters.first}')"
+        output << " unless Word.find_by text: '#{word}'"
+        output << "\n"
         last_word = word
       end
     end
